0bb90eae65f332e83f9d3b9ec088607878b05c84
Crowdbotics <team@crowdbotics.com>
2021-05-20 21:03:55 +0000

    Initial commit.

---
+++ backend/meregroceria_27008/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for meregroceria_27008 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meregroceria_27008.settings')
+
+application = get_wsgi_application()
